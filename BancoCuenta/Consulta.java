bbc42496b5d2e29d1e10b5964a6ba80e07441792
Mildret-Ventura <20243ds047@utez.edu.mx>
2025-09-04 22:15:44 -0600

    Hice la clase de Consulta

--- BancoCuenta/Consulta.java
+++
@@ -1,8 +0,0 @@
-public class Consulta {
-
-    public void mostrarSaldo(double saldo) {
-        // Lógica para mostrar el saldo
-        System.out.println("Su saldo es: " + saldo);
-    }
-    
-}
--- BancoCuenta/Retiro.java
+++ BancoCuenta/Retiro.java
@@ -1,7 +1,19 @@
-public class Retiro {
-    
-    public void realizarRetiro(double monto) {
-        
-        System.out.println("Retirando: " + monto);
+public class Retirar {
+    private double amount;
+
+    public Retirar(double amount) {
+        this.amount = amount;
+    }
+
+    public double execute(double currentBalance) {
+        System.out.println("Current balance: $" + currentBalance);
+        if (amount > 0 && amount <= currentBalance) {
+            double newBalance = currentBalance - amount;
+            System.out.println("Withdrawal successful. Remaining balance: $" + newBalance);
+            return newBalance;
+        } else {
+            System.out.println("Invalid withdrawal amount.");
+            return currentBalance;
+        }
     }
 }
