2c2f59be1825e88b6c790fd581926a9a0ba61737
Mildret-Ventura <20243ds047@utez.edu.mx>
2025-09-04 22:27:22 -0600

    Hice la clase de Deposito

--- BancoCuenta/Deposito.java
+++ BancoCuenta/Deposito.java
@@ -1,14 +1,23 @@
 public class Deposito {
-
-    public void realizarDeposito(CuentaBancaria cuenta, double cantidad) {
-        íf (cantidad > 0) {
-            cuenta.set(cantidad);
-        } else {
-            System.out.println("Cantidad de depósito inválida.");
-        }
+    private double cantidad;
 
 
-
+    public double getCantidad() {
+        return cantidad;
     }
-    
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void Depositar(CuentaBancaria cuenta){
+
+        if(cantidad > 0){
+        cuenta.setSaldo(cuenta.getSaldo() + cantidad);
+        System.out.println("Deposito exitoso");
+    } 
+    else {
+        System.out.println("Ingrese una cantidad valida para depositar");
+        }
+    }
 }
